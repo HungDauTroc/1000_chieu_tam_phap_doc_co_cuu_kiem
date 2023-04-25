71213f1833bded8726241cc45625416d8115c853
HungDauTroc <127832693+HungDauTroc@users.noreply.github.com>
2023-04-25 11:27:23 +0700

    Create bai 204.cpp

---
+++ Chuong_re_nhanh/bai 204.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+void enter(int arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> arr[i];
+	}
+}
+
+void summing(int arr[], int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int temp = arr[i];
+		int temp2 = arr[i - 1];
+		if (i == 0)
+		{
+			temp2 = arr[i];
+		}
+		if (temp > temp2)
+		{
+			sum = arr[i] + sum;
+		}
+	}
+	std::cout << "value is: " << sum;
+}
+int main()
+{
+	const int n = 7;
+	int Arr[n] = {0};
+	enter(Arr, n);
+	summing(Arr, n);
+	return 0;
+}
