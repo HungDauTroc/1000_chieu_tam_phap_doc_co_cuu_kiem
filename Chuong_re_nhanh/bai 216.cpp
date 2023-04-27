ff2ebc99043e876c9293badef2018d872d35ad35
HungDauTroc <127832693+HungDauTroc@users.noreply.github.com>
2023-04-27 10:25:59 +0700

    Create bai 216.cpp

---
+++ Chuong_re_nhanh/bai 216.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void enter(int arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> arr[i];
+	}
+}
+
+void count_Num(int arr[], int n)
+{
+	int count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
+			count++;
+		}
+	}
+	std::cout << count;
+}
+int main()
+{
+	const int n = 5;
+	int Arr[n] = { 0 };
+	enter(Arr, n);
+	count_Num(Arr, n);
+	return 0;
+}
