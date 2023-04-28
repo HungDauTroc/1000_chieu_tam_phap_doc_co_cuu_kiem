3750bc51c1ea5d4d1b7cc095f2fd36872cbe6128
HungDauTroc <127832693+HungDauTroc@users.noreply.github.com>
2023-04-28 15:27:20 +0700

    Create bai 226.cpp

---
+++ Chuong_re_nhanh/bai 226.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void enter(int arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> arr[i];
+	}
+}
+
+void search(int arr[], int n)
+{
+	int count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int temp = arr[i];
+		int temp2 = arr[i + 1];
+		if (i == n - 1)
+		{
+			temp2 = 0;
+		}
+		if (temp % 2 == 0 && temp2 % 2 == 0)
+		{
+			count++;
+		}
+	}
+	std::cout << count << "\t";
+}
+int main()
+{
+	const int n = 7;
+	int Arr[n] = { 0 };
+	enter(Arr, n);
+	search(Arr, n);
+	return 0;
+}
